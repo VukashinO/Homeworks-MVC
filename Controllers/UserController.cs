cb5d3125b1b2ffb7852f72491124ecc07b87596d
vukashinO <vukashin@hotmail.com>
2019-06-09 16:38:43 +0200

    second homework

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -48,5 +48,61 @@ namespace PizzaHut.Controllers
             }
             return View();
         }
+
+        public IActionResult Edit(int id)
+        {
+            var user = _users.Get(id);
+
+            EditUserViewModel editUserViewModel = new EditUserViewModel
+            {
+                ID = user.ID,
+                Name = user.Name,
+                Address = user.Address,
+                Email = user.Email,
+                City = user.City,
+                Phone = user.Phone
+
+
+            };
+            return View(editUserViewModel);
+        }
+
+        [HttpPost]
+
+        public IActionResult Edit(EditUserViewModel model)
+        {
+            var user = _users.Get(model.ID);
+           if(ModelState.IsValid)
+            {
+                user.Name = model.Name;
+                user.Address = model.Address;
+                user.Email = model.Email;
+                user.City = model.City;
+                _users.Update(user);
+                return RedirectToAction("details", user);
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var user = _users.Get(id);
+
+            return View(user);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(EditUserViewModel model)
+        {
+            var user = _users.Get(model.ID);
+            if(user != null)
+            {
+                _users.Delete(model.ID);
+                return RedirectToAction("index");
+            }
+
+            return View();
+            
+        }
     }
 }
